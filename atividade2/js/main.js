949a88485e09c7f0f6c5863cbe724a888470aad0
Mario Pilar <mrpilar@gmail.com>
2021-06-30 21:40:51 -0300

    Adicionado a validacao dos campos com JS

--- atividade2/js/main.js
+++ atividade2/js/main.js
@@ -1,3 +1,36 @@
-function clicou() {
-    alert("Cadastro Realizado com Sucesso! Agradecemos o contato!")
+
+function validar() {
+    var nome = document.getElementById("nome");
+    var email = document.getElementById("email");
+    var telefone = document.getElementById("telefone");
+    var mensagem = document.getElementById("mensagem");
+   
+    if(nome.value =="") {
+        alert("Nome não informado");
+        nome.focus();
+        return;
+    }
+
+    if (email.value == "") {
+        alert("E-mail não informado");
+        email.focus();
+        return;
+    }
+
+    if (telefone.value == "") {
+        alert("Telefone não informado");
+        telefone.focus();
+        return;
+    }
+
+    if (mensagem.value == "") {
+        alert("Escreva a sua mensagem");
+        telefone.focus();
+        return;
+    }
+
+    alert("Formulário enviado com sucesso!")
+    //formulario.submit();
+
+
 }
